3add07259fe5c33cd2e832b2b50b18aa065aa336
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-07-02 12:19:48 +0530

    UPDATE: utility functions created

---
+++ src/pages/Popup/utils/parseUrl.js
@@ -0,0 +1,15 @@
+import Orgs from "../datasets/news_orgs.json";
+
+function parseUrl(url) {
+  return new URL(url).hostname;
+}
+
+function matchOrganization(pageUrl, organizations = Orgs.list) {
+  var parsedUrl = new URL(pageUrl);
+  var hostname = parsedUrl.hostname;
+
+  var foundOrg = organizations.find((org) => org.hostname === hostname);
+  return foundOrg;
+}
+
+export { parseUrl, matchOrganization };
