03556b09d9775c3bbb832c8a748b40e99e8962f6
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-07-02 21:17:18 +0530

    UPDATE: code refactoring

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,14 +1,3 @@
-var h1 = document.querySelector("h1");
-if (h1 === null) {
-  h1 = document.createElement("h1");
-  h1.innerText = "Not found";
-}
-chrome.runtime.sendMessage(
-  { message: h1.innerText, type: "sending-h1" },
-  (response) => {
-    console.log("Heading sent", h1.innerText);
-  }
-);
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "send-h1") {
     var h1 = document.querySelector("h1");
