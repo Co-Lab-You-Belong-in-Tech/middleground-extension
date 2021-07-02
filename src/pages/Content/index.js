7300de92af2d3e2be37180396b0092a064e393f7
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-07-02 21:15:14 +0530

    UPDATE: code refactoring

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -6,14 +6,10 @@ if (h1 === null) {
 chrome.runtime.sendMessage(
   { message: h1.innerText, type: "sending-h1" },
   (response) => {
-    // 3. Got an asynchronous response with the data from the background
     console.log("Heading sent", h1.innerText);
-    // initializeUI(response);
   }
 );
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // console.log(request);
-  // console.log(sender);
   if (request.message === "send-h1") {
     var h1 = document.querySelector("h1");
     if (h1 === null) {
