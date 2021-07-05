a08476db3f4e8bb5df2390cd79a6f912d35e1112
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-07-05 21:34:19 +0530

    ADD: filter function; UPDATE: news org logo position in json

---
+++ src/pages/Popup/utils/assignOrgBias.js
@@ -0,0 +1,25 @@
+import { parseUrl } from "./parseUrl";
+import Orgs from "../datasets/news_orgs.json";
+
+function assignBias(articles, organizations = Orgs.list) {
+  articles = articles.map(function (article) {
+    const hostname = parseUrl(article.url);
+    var foundOrg = organizations.find((org) => org.hostname === hostname);
+    var biasImage;
+    if (foundOrg.bias === "center") {
+      biasImage = "./resources/center_mini.png";
+    } else if (foundOrg.bias === "left leaning") {
+      biasImage = "./resources/left_mini.png";
+    } else {
+      biasImage = "./resources/right_mini.png";
+    }
+    return {
+      ...article,
+      bias: foundOrg.bias,
+      biasImage,
+    };
+  });
+  return articles;
+}
+
+export { assignBias };
